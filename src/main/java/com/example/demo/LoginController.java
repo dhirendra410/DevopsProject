388bef51cd425dae2f55a98732eb2c7fd55e7772
sollogics <dhirendra410@gmail.com>
2019-07-23 22:48:23 +0530

    First Commit

---
+++ src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LoginController {
+
+	
+
+	
+	static int x;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public void activate() {
+		
+		System.out.println("HIII");
+
+	}
+	
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcome(){
+		return "Hi devops";
+	}
+	
+
+}
